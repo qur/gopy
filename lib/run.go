330a7f87f64f0d256d3c4376e6c8d840c80d133c
Julian Phillips <julian@quantumfyre.co.uk>
2011-08-02 18:35:41 +0100

    Start implementing PyRun_ functions

---
+++ lib/run.go
@@ -0,0 +1,53 @@
+// Copyright 2011 Julian Phillips.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package py
+
+// #include <Python.h>
+// #include "utils.h"
+import "C"
+
+import (
+	"os"
+	"unsafe"
+)
+
+type StartToken int
+
+const (
+	EvalInput StartToken = iota
+	FileInput
+	SingleInput
+)
+
+func Run_File(filename string, start StartToken, globals, locals Object) (Object, os.Error) {
+	name := C.CString(filename)
+	defer C.free(unsafe.Pointer(name))
+
+	mode := C.CString("r")
+	defer C.free(unsafe.Pointer(mode))
+
+	var token C.int
+	switch start {
+	case EvalInput:
+		token = C.Py_eval_input
+	case FileInput:
+		token = C.Py_file_input
+	case SingleInput:
+		token = C.Py_single_input
+	}
+
+	file, err := C.fopen(name, mode)
+	if file == nil {
+		return nil, err
+	}
+	defer C.fclose(file)
+
+	obj := C.PyRun_FileExFlags(file, name, token, c(globals), c(locals), 0, nil)
+	if obj == nil {
+		return nil, exception()
+	}
+
+	return newBaseObject(obj).actual(), nil
+}
