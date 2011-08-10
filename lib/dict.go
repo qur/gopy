6b2781e68d8fdad48def546a0885ecb9a7310070
Julian Phillips <julian@quantumfyre.co.uk>
2011-08-10 21:45:30 +0100

    Add some more documentation
    At the moment, we are sorely lacking docmentation.  Start adding some.
    
    Also tweak a couple of *Dict methods to return *List instead of
    Object, so that the user can immediately access list behaviour on the
    returned value - without necessity of a type assertion.

--- lib/dict.go
+++ lib/dict.go
@@ -15,6 +15,9 @@ import (
 	"unsafe"
 )
 
+// *Dict represents a Python dictionary.  In addition to satisfying the Object
+// interface, Dict pointers also have a number of methods defined - representing
+// the PyDict_XXX functions from the Python C API.
 type Dict struct {
 	BaseObject
 }
@@ -30,6 +33,9 @@ func newDict(obj *C.PyObject) *Dict {
 	return (*Dict)(unsafe.Pointer(obj))
 }
 
+// Create a new empty dictionary.
+//
+// Return value: New Reference.
 func Dict_New() (*Dict, os.Error) {
 	ret := C.PyDict_New()
 	if ret == nil {
@@ -54,25 +60,36 @@ func (d *Dict) CheckExact() bool {
 	return false
 }
 
+// Clear empties the dictionary d of all key-value pairs.
 func (d *Dict) Clear() {
 	C.PyDict_Clear(c(d))
 }
 
+// Contains Returns true if the dictionary contains the given key.  This is
+// equivalent to the Python expression "key in d".
 func (d *Dict) Contains(key Object) (bool, os.Error) {
 	ret := C.PyDict_Contains(c(d), c(key))
 	return int2BoolErr(ret)
 }
 
+// Copy returns a new dictionary that contains the same key-values pairs as d.
+//
+// Return value: New Reference.
 func (d *Dict) Copy() (Object, os.Error) {
 	ret := C.PyDict_Copy(c(d))
 	return obj2ObjErr(ret)
 }
 
+// SetItem inserts "val" into dictionary d with the key "key".  If "key" is not
+// hashable, then a TypeError will be returned.
 func (d *Dict) SetItem(key, val Object) os.Error {
 	ret := C.PyDict_SetItem(c(d), c(key), c(val))
 	return int2Err(ret)
 }
 
+// SetItemString inserts "val" into dictionary d with the key "key" (or rather,
+// with a *String with the value of "key" will be used as the key).  If "key" is
+// not hashable, then a TypeError will be returned.
 func (d *Dict) SetItemString(key string, val Object) os.Error {
 	s := C.CString(key)
 	defer C.free(unsafe.Pointer(s))
@@ -80,11 +97,16 @@ func (d *Dict) SetItemString(key string, val Object) os.Error {
 	return int2Err(ret)
 }
 
+// DelItem removes the entry with the key of "key" from the dictionary d.  If
+// "key" is not hashable, a TypeError is returned.
 func (d *Dict) DelItem(key Object) os.Error {
 	ret := C.PyDict_DelItem(c(d), c(key))
 	return int2Err(ret)
 }
 
+// DelItem removes the entry with the key of "key" (or rathery, with a *String
+// with the value of "key" will be used as the key) from the dictionary d.  If
+// "key" is not hashable, a TypeError is returned.
 func (d *Dict) DelItemString(key string) os.Error {
 	s := C.CString(key)
 	defer C.free(unsafe.Pointer(s))
@@ -92,6 +114,10 @@ func (d *Dict) DelItemString(key string) os.Error {
 	return int2Err(ret)
 }
 
+// GetItem returns the Object from dictionary d which has the key "key".  If
+// there is no such Object, then nil is returned (without an error).
+//
+// Return value: Borrowed Reference.
 func (d *Dict) GetItem(key Object) (Object, os.Error) {
 	ret := C.PyDict_GetItem(c(d), c(key))
 	return obj2ObjErr(ret)
@@ -104,21 +130,35 @@ func (d *Dict) GetItemString(key string) (Object, os.Error) {
 	return obj2ObjErr(ret)
 }
 
-func (d *Dict) Items() (Object, os.Error) {
+// Items returns a *List containing all the items from the dictionary d, as with
+// the Python "d.items()".
+//
+// Return value: New Reference.
+func (d *Dict) Items() (*List, os.Error) {
 	ret := C.PyDict_Items(c(d))
-	return obj2ObjErr(ret)
+	return newList(ret), exception()
 }
 
-func (d *Dict) Keys() (Object, os.Error) {
+// Keys returns a *List containing all the keys from the dictionary d, as with
+// the Python "d.keys()".
+//
+// Return value: New Reference.
+func (d *Dict) Keys() (*List, os.Error) {
 	ret := C.PyDict_Keys(c(d))
-	return obj2ObjErr(ret)
+	return newList(ret), exception()
 }
 
-func (d *Dict) Values() (Object, os.Error) {
+// Values returns a *List containing all the values from the dictionary d, as
+// with the Python "d.values()".
+//
+// Return value: New Reference.
+func (d *Dict) Values() (*List, os.Error) {
 	ret := C.PyDict_Values(c(d))
-	return obj2ObjErr(ret)
+	return newList(ret), exception()
 }
 
+// Size returns the number of items in the dictionary d.  This is equivalent to
+// the Python "len(d)".
 func (d *Dict) Size() int64 {
 	ret := C.PyDict_Size(c(d))
 	if ret < 0 {
@@ -152,6 +192,9 @@ func (d *Dict) MergeFromSeq2(o Object, override bool) os.Error {
 	return int2Err(ret)
 }
 
+// Map returns a Go map that contains the values from the Python dictionary,
+// indexed by the keys.
+//
 // Note: the map holds borrowed references
 func (d *Dict) Map() map[Object]Object {
 	m := make(map[Object]Object, d.Size())
@@ -166,6 +209,9 @@ func (d *Dict) Map() map[Object]Object {
 	return m
 }
 
+// MapString is similar to Map, except that the keys are first converted to
+// strings.  If the keys are not all Python strings, then an error is returned.
+//
 // Note: the map holds borrowed references
 func (d *Dict) MapString() (map[string]Object, os.Error) {
 	m := make(map[string]Object, d.Size())
@@ -184,6 +230,7 @@ func (d *Dict) MapString() (map[string]Object, os.Error) {
 	return m, nil
 }
 
+// String returns a string representation of the contents of the dictionary d.
 func (d *Dict) String() string {
 	if d == nil {
 		return "<nil>"
