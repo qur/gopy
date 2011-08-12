7e39cfae4bf69ae2bd5e7c7809c0698fb9c75bb0
Julian Phillips <julian@quantumfyre.co.uk>
2011-08-13 00:35:43 +0100

    Finally implement PackTuple
    Finally got around to implementing PackTuple.  This function
    replicates the functionality of the C function rather than calling it.
    This is because the function is a) fairly trivial, b) rather hard to
    call from Go.  Rather than adding a lot of overhead to the call, we
    simply do the work in Go ourselves.  Though we do have to do some
    messy pointer manipulation as Go won't let us use indexing.

--- lib/tuple.go
+++ lib/tuple.go
@@ -7,6 +7,7 @@ package py
 // #include "utils.h"
 // static inline int tupleCheck(PyObject *o) { return PyTuple_Check(o); }
 // static inline int tupleCheckE(PyObject *o) { return PyTuple_CheckExact(o); }
+// static inline size_t tupleItemSize() { return sizeof(PyObject *); }
 import "C"
 
 import (
@@ -54,8 +55,32 @@ func NewTuple(size int64) (*Tuple, os.Error) {
 }
 
 func PackTuple(items ...Object) (*Tuple, os.Error) {
-	// TODO: ...
-	return nil, fmt.Errorf("Not Implemented")
+	ret := C.PyTuple_New(C.Py_ssize_t(len(items)))
+	if ret == nil {
+		return nil, exception()
+	}
+
+	// Since the ob_item array has a size of 1, Go won't let us index more than
+	// a single entry, and if we try and use our own local type definition with
+	// a flexible array member then cgo converts it to [0]byte which is even
+	// less useful.  So, we resort to pointer manipulation - which is
+	// unfortunate, as it's messy in Go.
+
+	// base is a pointer to the first item in the array of PyObject pointers.
+	// step is the size of a PyObject * (i.e. the number of bytes we need to add
+	// to get to the next item).
+	base := unsafe.Pointer(&(*C.PyTupleObject)(unsafe.Pointer(ret)).ob_item[0])
+	step := uintptr(C.tupleItemSize())
+
+	for _, item := range items {
+		item.Incref()
+		*(**C.PyObject)(base) = c(item)
+
+		// Move base to point to the next item, by incrementing by step bytes
+		base = unsafe.Pointer(uintptr(base) + step)
+	}
+
+	return newTuple(ret), nil
 }
 
 func (t *Tuple) CheckExact() bool {
