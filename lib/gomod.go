708d2b9b7f07cf125372e94eda2fcadeef2f31d4
Julian Phillips <julian@quantumfyre.co.uk>
2011-08-13 12:22:00 +0100

    Make it possible to use a channel from Python
    Channels are useful things for communicating.  Make it possible to
    communicate with Python code by wrapping a "chan Object" into a Chan,
    an object that can be used in Python.
    
    The Python object has put and get methods (modelled after the Queue
    type) that allow Python to send and recieve on the underlying channel.
    
    We also introduce a "go" module that contains the Chan type from the
    Python side.  The "go" module must be initialized before the new Chan
    type can be used.

---
+++ lib/gomod.go
@@ -0,0 +1,50 @@
+// Copyright 2011 Julian Phillips.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package py
+
+// #include "utils.h"
+import "C"
+
+import (
+	"os"
+)
+
+var goModule *Module
+
+// InitGoModules initializes (and returns) the special built-in "go" module.
+// This module exports go specific items to Python that can be accessed by
+// calling "import go".  Since this function also initialises the Python
+// representations that are exported by the "go" module it must be called before
+// any of these types (e.g. Chan) can be used.
+//
+// This function may be called more than once to get at the *Module, the module
+// will only be created and initialized once.
+func InitGoModule() (*Module, os.Error) {
+	if goModule != nil {
+		return goModule, nil
+	}
+
+	goModule, err := InitModule("go", []Method{})
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := chanClass.Create()
+	if err != nil {
+		goModule.Decref()
+		goModule = nil
+		return nil, err
+	}
+
+	err = goModule.AddObject("Chan", c)
+	if err != nil {
+		goModule.Decref()
+		goModule = nil
+		c.Decref()
+		return nil, err
+	}
+
+	return goModule, nil
+}
