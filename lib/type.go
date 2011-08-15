5762875ad9a9e5ab7a762cf49ee3104b5d40961e
Julian Phillips <julian@quantumfyre.co.uk>
2011-08-15 07:56:39 +0100

    Add some PyType_XXX functions

--- lib/type.go
+++ lib/type.go
@@ -56,3 +56,40 @@ func (t *Type) String() string {
 	}
 	return C.GoString(C.PyString_AsString(pyS))
 }
+
+// CheckExact returns true when "t" is an actual Type object, and not some form
+// of subclass.
+func (t *Type) CheckExact() bool {
+	return C.typeCheckE(c(t)) == 1
+}
+
+// PyType_ClearCache : TODO - ???
+
+// Modified should be called after the attributes or base class of a Type have
+// been changed.
+func (t *Type) Modified() {
+	C.PyType_Modified(&t.o)
+}
+
+// HasFeature returns true when "t" has the feature in question.
+func (t *Type) HasFeature(feature uint32) bool {
+	return t.o.tp_flags & C.long(feature) != 0
+}
+
+// IsGc returns true if the type "t" supports Cyclic Garbage Collection.
+func (t *Type) IsGc() bool {
+	return t.HasFeature(TPFLAGS_HAVE_GC)
+}
+
+// IsSubtype returns true if "t" is a subclass of "t2".
+func (t *Type) IsSubtype(t2 *Type) bool {
+	return C.PyType_IsSubtype(&t.o, &t2.o) == 1
+}
+
+// PyType_GenericAlloc : This is an internal function, which we should not need
+// to expose.
+
+// PyType_GenericNew : Another internal function we don't need to expose.
+
+// PyType_Ready : This function is wrapped (along with a lot of other
+// functionality) in the Create method of the Class stuct.
