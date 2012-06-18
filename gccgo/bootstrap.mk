d22c522c8c5c428a9d78f5989508f8f913394139
Julian Phillips <julian@quantumfyre.co.uk>
2012-06-18 21:10:44 +0100

    Move VC only files to separate make file
    There are some commands that need to be inside VC, and some rules that
    will break things if included outside of VC.  So move a bunch of stuff
    from the Makefile to a separate file that isn't included in the
    tarball.  The Makefile can then -include this file, so it will be
    ignored if not available.

---
+++ gccgo/bootstrap.mk
@@ -0,0 +1,29 @@
+VERSION=$(shell git describe --match 'ext/v*' --dirty | cut -c6-)
+
+gofiles: $(GOFILES) utils.c utils.h
+
+%.go:: ../lib/%.go t/decgo typedefs.map gen-py.go
+	@$(ECHO) "  DE-CGO\t$@"
+	./t/decgo $< $@ || (rm $@; exit 1)
+
+utils.c: ../lib/utils.c utils.h
+	@$(ECHO) "  COPY\t\t$@"
+	cp $< $@
+
+utils.h: ../lib/utils.h
+	@$(ECHO) "  COPY\t\t$@"
+	cp $< $@
+
+gopy-$(VERSION).tgz: $(GOFILES) utils.c utils.h
+	@$(ECHO) "  TAR\t\t$@"
+	tar --transform "s/^\./gopy-$(VERSION)/" --exclude '*.fixup' \
+		--exclude '.gitignore' --exclude 'gopy-*.tgz' \
+		--exclude 'bootstrap.mk' -czf $@ .
+
+tarball: gopy-$(VERSION).tgz
+
+deepclean: clean
+	@$(ECHO) "  DEEPCLEAN"
+	rm -f $(GOFILES) utils.[ch]
+
+.PHONY: gofiles tarball deepclean
