60a19c273c04e4389a9685c3ba65e71240f4152d
Julian Phillips <julian@quantumfyre.co.uk>
2023-02-11 19:30:16 +0000

    Remove stub header after running
    Leaving the stub header around seems to be causing compilation issues?
    We don't need the header after generating exc.go, so remove it at the
    end of the script.

--- gen_exc.py
+++ gen_exc.py
@@ -4,6 +4,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import os
 import sys
 import re
 import subprocess
@@ -74,6 +75,7 @@ def main():
         t.start()
         p.wait()
         t.join()
+    os.remove("_cgo_export.h")
 
 
 if __name__ == "__main__":
