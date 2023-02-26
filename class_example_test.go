268361e1b765dc321f7e6905cb1ad54266915507
Julian Phillips <julian@quantumfyre.co.uk>
2023-02-26 22:45:13 +0000

    Add some comments to example

--- class_example_test.go
+++ class_example_test.go
@@ -6,10 +6,15 @@ import (
 	"gopython.xyz/py/v3"
 )
 
+// myClass is the type that represents the custom class in Go.
 type myClass struct {
 	py.ClassBaseObject
 }
 
+// Call is the method called when the type is called in Python.
+//
+// This is one of many optional functions that can be implemented on the custom
+// class type to respond to actions in Python.
 func (m *myClass) Call(args *py.Tuple, kwds *py.Dict) (py.Object, error) {
 	var s string
 	if err := py.ParseTuple(args, "s", &s); err != nil {
@@ -21,7 +26,8 @@ func (m *myClass) Call(args *py.Tuple, kwds *py.Dict) (py.Object, error) {
 	return py.ReturnNone(), nil
 }
 
-var myClassDef = py.Class{
+// myClassType is a py.Class value that is the Type of myClass.
+var myClassType = py.Class{
 	Name:   "MyClass",
 	Flags:  py.ClassBaseType,
 	Doc:    "An example Class",
@@ -32,13 +38,14 @@ func ExampleClass() {
 	lock := py.InitAndLock()
 	defer lock.Finalize()
 
-	if err := myClassDef.Create(); err != nil {
+	// we have to Create the Class before we can use it
+	if err := myClassType.Create(); err != nil {
 		fmt.Printf("ERROR: %s", err)
 		return
 	}
 
 	// Python: "m = MyClass()"
-	m, err := myClassDef.CallGo(nil, nil)
+	m, err := myClassType.CallGo(nil, nil)
 	if err != nil {
 		fmt.Printf("ERROR: %s", err)
 		return
