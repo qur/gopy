8bfcb43a9ebabf2faf1cec0d7a8bdbd124bc12da
Julian Phillips <julian@quantumfyre.co.uk>
2026-05-10 13:14:56 +0100

    Add error handling to the Long conversions
    The Python C API long conversion routines can return errors, but they
    were being ignored previously.
    
    Also switch to using the newer routines that not only use less confusing
    types, but also make it much easier to see when an error occured.

--- long.go
+++ long.go
@@ -7,18 +7,94 @@ import (
 	"strconv"
 )
 
-func NewLong[T ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32](i T) *Long {
-	return newLong(C.PyLong_FromLongLong(C.longlong(int64(i))))
+// NewLongChecked return a new Long with the given value, or an error if an
+// exception was raised.
+//
+// Return value: New Reference.
+func NewLongChecked[T ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64](i T) (*Long, error) {
+	if i >= 0 {
+		return newLong(C.PyLong_FromUInt64(C.uint64_t(uint64(i)))), exception()
+	}
+
+	return newLong(C.PyLong_FromInt64(C.int64_t(int64(i)))), exception()
 }
 
+// NewLong returns a new Long with the given value, it will panic if an
+// exception is raised.
+//
+// Return value: New Reference.
+func NewLong[T ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64](i T) *Long {
+	l, err := NewLongChecked(i)
+	if err != nil {
+		panic(err)
+	}
+
+	return l
+}
+
+// AsInt64 returns the value of l as an int64. If the value does not fit in an
+// int64, then OverflowError will be returned.
+func (l *Long) AsInt64() (int64, error) {
+	var v C.int64_t
+
+	if err := int2Err(C.PyLong_AsInt64(c(l), &v)); err != nil {
+		return 0, err
+	}
+
+	return int64(v), nil
+}
+
+// Int64 returns the value of l as an int64, it will panic if AsInt64 would
+// return an error.
 func (l *Long) Int64() int64 {
-	return int64(C.PyLong_AsLongLong(c(l)))
+	i, err := l.AsInt64()
+	if err != nil {
+		panic(err)
+	}
+
+	return i
 }
 
+// AsUint64 returns the value of l as a uint64. If the value does not fit in an
+// uint64, then OverflowError will be returned, if the value is negative, then
+// ValueError will be returned.
+func (l *Long) AsUint64() (uint64, error) {
+	var v C.uint64_t
+
+	if err := int2Err(C.PyLong_AsUInt64(c(l), &v)); err != nil {
+		return 0, err
+	}
+
+	return uint64(v), nil
+}
+
+// Uint64 returns the value of l as a uint64, it will panic if AsUint64 would
+// return an error.
+func (l *Long) Uint64() uint64 {
+	i, err := l.AsUint64()
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+// String returns a string representation of the value l. It will panic if l
+// can't be converted to either an in64 or a uint64.
 func (l *Long) String() string {
 	if l == nil {
 		return nilValue
 	}
 
-	return strconv.FormatInt(l.Int64(), 10)
+	i64, err := l.AsInt64()
+	if err == nil {
+		return strconv.FormatInt(i64, 10)
+	}
+
+	u64, err := l.AsUint64()
+	if err == nil {
+		return strconv.FormatUint(u64, 10)
+	}
+
+	panic(NewErrorV(ValueError, l))
 }
